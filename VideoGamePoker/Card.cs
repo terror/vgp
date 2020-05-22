870b06c366f4ec53a3abb2322c6d7a1b88661716
Liam <terroridk@gmail.com>
2020-05-21 21:39:20 -0400

    initial commit

---
+++ VideoGamePoker/Card.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace VideoGamePoker
+{
+    public class Card
+    {
+
+        public enum FaceValue { Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace };
+        // Note: Ace is both the lowest and highest card
+        public enum Suit { Hearts, Diamonds, Clubs, Spades };
+
+        private FaceValue _faceValue;
+        private Suit _suit;
+
+        public Card()
+        {
+            _suit = Suit.Spades;
+            _faceValue = FaceValue.Ace;
+        }
+
+        public Card(Suit theSuit_, FaceValue theFaceValue_)
+        {
+            _suit = theSuit_;
+            _faceValue = theFaceValue_;
+        }
+
+        public override string ToString()
+        {
+            return _faceValue + " of " + _suit;
+        }
+
+        public Suit GetSuit()
+        {
+            return _suit;
+        }
+
+        public FaceValue GetFaceValue()
+        {
+            return _faceValue;
+        }
+    }
+}
---
+++ VideoGamePoker/Deck.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+
+namespace VideoGamePoker
+{
+    public class Deck
+    {
+        private const int TOTAL_CARDS = 52;
+
+        private Card[] deck = new Card[TOTAL_CARDS];
+
+        private int currentCard = 0;
+
+        private Random random;
+
+        public Deck()
+        {
+            random = new Random();      //generate a list of random numbers to be used in the shuffle
+
+            int num = 0;
+            foreach (Card.Suit aSuitValue in Enum.GetValues(typeof(Card.Suit)))
+            {
+                foreach (Card.FaceValue aFaceValue in Enum.GetValues(typeof(Card.FaceValue)))
+                {
+                    deck[num++] = new Card(aSuitValue, aFaceValue);
+                }
+            }
+        }
+
+        public void Shuffle()
+        {
+            currentCard = 0;            //reset the currentCard index for each round. 
+            int randomPosition;
+
+            for (int i = 0; i < deck.Length; i++)
+            {
+                randomPosition = random.Next(deck.Length);
+                Swap(deck, i, randomPosition);
+            }
+        }
+
+        //Swap method
+        //Input: Card array, int cardPosition, int cardPositionTwo
+        //Output: none
+        //Cards are objects and the array of cards holds the address (references) of those Card objects.
+        //Swapping method changes the addresses of those cards in the array. 
+        private void Swap(Card[] deck, int cardPositionOne, int cardPositionTwo)
+        {
+            Card temp = deck[cardPositionOne];
+            deck[cardPositionOne] = deck[cardPositionTwo];
+            deck[cardPositionTwo] = temp;
+        }
+
+        //DealACard method
+        //Pre-Condtion - the deck cannot be empty.
+        //IsEmpty is verified in the application before calling DealACard()
+        //Input: none
+        //Outputs: none
+        //Returns the next card in the deck. 
+        public Card DealACard()
+        {
+            return deck[currentCard++];
+        }
+
+        public bool IsEmpty()
+        {
+            return currentCard > TOTAL_CARDS - 1;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            foreach (Card c in deck)
+            {
+                builder.AppendLine(c.ToString());
+            }
+
+            return builder.ToString();
+        }
+    }
+}
